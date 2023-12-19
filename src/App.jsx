7dc82d8b90ff9b6e48a1911531dad967749a46a9
unknown <dubeypratham11@gmail.com>
2023-12-19 20:33:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './component/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="text-8xl text-white">
+      <nav>
+        <ul>
+          <li><Link>Home</Link></li>
+          <li><Link>Home</Link></li>
+          <li><Link>Home</Link></li>
+          <li><Link>Home</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
