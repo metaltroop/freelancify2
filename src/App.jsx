d9a36ea19f43aa3e6b64fa19cb022cf511e6f376
unknown <dubeypratham11@gmail.com>
2023-12-24 18:42:04 +0530

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import Signinup from "./pages/SignINUP/Signinup";
+
 import Landing from "./pages/Landing";
 import Signin from "./pages/Login/Signin";
 import Signup from "./pages/Register/Signup";
@@ -13,7 +13,6 @@ function App() {
 
   return (
     <div className="min-h-screen w-[100%]">
-      <Landing/>
       <Routes location={location}>
         <Route
           path="/Signin"
@@ -25,7 +24,6 @@ function App() {
             <Signup direction={direction} setDirection={setDirection} />
           }
         />
-        <Route path="/signinup" element={<Signinup />} />
         <Route path="/Landing" element={<Landing />} />
         <Route path="/Signin" element={<Signin />} />
       
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,29 @@
-import Navbar from '../component/Navbar'
+import Navbar from "../component/Navbar";
+import hero from "../assets/landinghero.png";
 
 function Landing() {
   return (
-    <Navbar/>
-  )
+    <div className="h-screen bg-[#F2FAFA]">
+      <div className="h-32">
+        <Navbar />
+      </div>
+      <div className="h-[100%] flex justify-evenly items-center p-8">
+        <div className="">
+          <h1 className="text-8xl font-[700] text-[#252525] ">
+            Are You Looking For <br /> Freelancers?
+          </h1>
+          <p className="text-[24px] font-[400] text-[#252525]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex,
+            impedit.
+          </p>
+        </div>
+        <div className="w-[800px] h-[800px]">
+          {" "}
+          <img src={hero} alt="Side" className=""></img>{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
